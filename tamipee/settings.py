91494afed28eb61394a6e6081e3d3e0138a689b2
Deejulu <davidnetflix0011@gmail.com>
2026-06-06 11:16:01 +0100

    Fix production OTP leak by defaulting DEBUG to False

--- tamipee/settings.py
+++ tamipee/settings.py
@@ -38,7 +38,8 @@ def env_list(name, default=''):
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env_bool('DJANGO_DEBUG', True)
+DEBUG = env_bool('DJANGO_DEBUG', False)
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
